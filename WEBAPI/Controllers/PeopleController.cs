c600f5038c9601b7efb57919c5e8b55e868f426b
vudomc1012 <vudomc1012@gmail.com>
2022-06-22 10:10:26 +0700

    Add project files.

---
+++ WEBAPI/Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace WEBAPI.Controllers
+{
+    public class PeopleController : Controller
+    {
+        //Get people
+        public IActionResult Index()
+        {
+            string path = "https://randomuser.me/api/?results=15";
+            object people = GetPeople(path);
+            JObject jObject = JObject.Parse(people.ToString());
+            ViewBag.data = jObject["results"];
+            return View();
+        }
+
+        public object GetPeople(string path)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                return JsonConvert.DeserializeObject<Object>(
+                    webClient.DownloadString(path)
+            );
+            }
+        }
+    }
+}
